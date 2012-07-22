ecb103b71368872818e8d7a052c11cf337f96059
bigwheel <k.bigwheel+eng@gmail.com>
2012-07-22 20:46:45 +0900

    Append mock test to SearchResult Model.

--- spec/models/search_result_spec.rb
+++ spec/models/search_result_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe SearchResult, "Model" do
-  subject do
-    #stub(OpenURI).open_uri { open('./spec/dummy/acidic_slime.html', 'r') }
-  end
-
   describe '#urls' do
-    subject { SearchResult.new(set: 'Magic 2013').urls }
+    let(:params) { { set: 'Magic 2013' } }
+    subject do
+      base_url = SearchUrl.new(params)
+      mock(OpenURI).open_uri(base_url.concat).
+        returns(open('./spec/dummy/search_results/top.html', 'r'))
+      (0...10).each do |page_number|
+        mock(OpenURI).open_uri(base_url.append_params(page: page_number).concat).
+          returns(open("./spec/dummy/search_results/#{page_number}.html", 'r'))
+      end
+      SearchResult.new(params).urls
+    end
     its(:length) { should == 234 }
     it { subject.uniq.length.should == 234 }
   end
