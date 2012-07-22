ce0a45b80d5268729081128445ace68ac12a1af0
bigwheel <k.bigwheel+eng@gmail.com>
2012-07-22 16:45:47 +0900

    Divide big test into many 'its' test

--- spec/models/card_property_spec.rb
+++ spec/models/card_property_spec.rb
@@ -6,23 +6,31 @@ describe CardProperty, "Model" do
   it 'can be created' do
     should_not be_nil
   end
-  it do
-    acidic_slime = { card_name: 'Acidic Slime', mana_cost: [3, :green, :green],
-      converted_mana_cost: 5, types: 'Creature  — Ooze',
-      card_text: '<div class="cardtextbox">Deathtouch <i>' +
-      '(Any amount of damage this deals to a creature is ' +
-      'enough to destroy it.)</i></div><div class="cardte' +
-      'xtbox">When Acidic Slime enters the battlefield, d' +
-      'estroy target artifact, enchantment, or land.</div>',
-      p_t: '2 / 2', expansion: 'Magic 2013',
-      rarity: 'Uncommon', all_sets: ['Magic 2010 (Uncommon)',
-        'Magic 2011 (Uncommon)', 'Magic 2012 (Uncommon)',
-        'Magic 2013 (Uncommon)',
-        'Magic: The Gathering-Commander (Uncommon)'],
-        card_number: 159, artist: 'Karl Kopinski' }
-
-    stub(OpenURI).open_uri { open('./spec/dummy/acidic_slime.html', 'r') }
-    described_class.create_from_id(265718).should == described_class.new(acidic_slime)
+  describe 'Acidic Slime' do
+    subject do
+      stub(OpenURI).open_uri { open('./spec/dummy/acidic_slime.html', 'r') }
+      described_class.create_from_id(265718)
+    end
+    its(:card_name) { should == 'Acidic Slime' }
+    its(:mana_cost) { should == ['3', 'Green', 'Green'] }
+    its(:converted_mana_cost) { should == 5 }
+    its(:types) { should == 'Creature  — Ooze' }
+    its(:card_text) do
+      should == '<div class="cardtextbox">Deathtouch <i>' +
+        '(Any amount of damage this deals to a creature is ' +
+        'enough to destroy it.)</i>' + "\n</div>\n" + '<div class="cardte' +
+        'xtbox">When Acidic Slime enters the battlefield, d' +
+        'estroy target artifact, enchantment, or land.</div>'
+    end
+    its(:flavor_text) { should == '' }
+    its(:p_t) { should == '2 / 2' }
+    its(:expansion) { should == 'Magic 2013' }
+    its(:rarity) { should == 'Uncommon' }
+    its(:all_sets) { should == ['Magic 2010 (Uncommon)', 'Magic 2011 (Uncommon)',
+                                'Magic 2012 (Uncommon)', 'Magic 2013 (Uncommon)',
+                                'Magic: The Gathering-Commander (Uncommon)'] }
+    its(:card_number) { should == 159 }
+    its(:artist) { should == 'Karl Kopinski' }
   end
   it do
     expect { subject.value_from_label_name }.to raise_error(NoMethodError)
