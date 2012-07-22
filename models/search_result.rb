1109ef48683215759d03bb624baf34c3648d006d
bigwheel <k.bigwheel+eng@gmail.com>
2012-07-22 19:33:38 +0900

    Complete SearchResult Model implementation

--- models/search_result.rb
+++ models/search_result.rb
@@ -1,16 +1,30 @@
 class SearchResult
+  attr_reader :urls
   def initialize params
-    @doc = Nokogiri::HTML(open(SearchUrl.new(params).concat))
-  end
-  def number
-    @number ||= _number
+    base_url = SearchUrl.new(params)
+    doc = Nokogiri::HTML(open(base_url.concat))
+
+    page_size = (result_number(doc) / 25.0).ceil
+
+    @urls = (0...page_size).map do |page_number|
+      multiverseids(base_url, page_number)
+    end.flatten
   end
   private
-  def _number
-    span_tag = @doc.at_xpath("//div[@class='contentTitle']" +
-                             "[contains(text(), 'Search:')]/span")
+  def result_number doc
+    span_tag = doc.at_xpath("//div[@class='contentTitle']" +
+                            "[contains(text(), 'Search:')]/span")
     span_tag.at_xpath('i').unlink # remove a child <i> ~ </i> node
     match_data = span_tag.content.strip.match(/\((\d+)\)/)
     match_data[1].to_i
+  end
+  def multiverseids(base_url, page_number)
+    a_search_result_page = base_url.append_params(page: page_number)
+    doc = Nokogiri::HTML(open(a_search_result_page.concat))
+    doc.xpath("//span[@class='cardTitle']/a/@href").map do |href|
+      prefix = Regexp.escape %!../Card/Details.aspx?multiverseid=!
+      match_data = href.content.match(/#{prefix}(\d+)/)
+      match_data[1].to_i
+    end
   end
 end
