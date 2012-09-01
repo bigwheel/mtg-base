e28185a2f533936f7b91008917d55a477e4811e8
bigwheel <k.bigwheel+eng@gmail.com>
2012-09-01 21:59:07 +0900

    Update data fetch spec and API

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -3,7 +3,10 @@ MtgPackGenerator.controller do
     render 'index'
   end
   get :cardproperty do
-    CardProperty.all.to_json
+    CardProperty.where(expansion: 'Magic 2013').map do |card_property|
+      { rarity: card_property.rarity,
+        multiverseid: card_property.multiverseid }
+    end.to_json
     #M12CardProperty.only(:multiverseid).map {|cp| cp.multiverseid}.to_json
   end
   get :cardproperty, with: :multiverseid do
--- spec/models/card_property_spec.rb
+++ spec/models/card_property_spec.rb
@@ -2,19 +2,24 @@
 require 'spec_helper'
 
 describe CardProperty, 'Model', :vcr => { :cassette_name => 'gatherer/card', :record => :new_episodes } do
-  before { described_class.delete_all }
-  context 'when M13 cards are given' do
-    it 'should not raise error' do
-      card_urls = GathererScraper::search_result(set: 'Magic 2013')
-      expect do
-        card_urls.each do |card_url|
-          begin
-            described_class::parse(card_url).save!
-          rescue => e
-            raise e.exception(e.message + e.backtrace.join("\n") + " + card_url: #{card_url}")
+  before(:all) { described_class.delete_all }
+  shared_examples_for :expansion do |cardset_name|
+    context "when #{cardset_name} cards are given", :vcr => { :cassette_name => "gatherer/cardset/#{cardset_name}", :record => :new_episodes } do
+      subject { GathererScraper::search_result(set: cardset_name) }
+      it 'should not raise error' do
+        expect do
+          subject.each do |url|
+            begin
+              described_class.parse(url).save!
+            rescue => e
+              raise e.exception(e.inspect + " + url: #{url}")
+            end
           end
-        end
-      end.not_to raise_error
+        end.not_to raise_error
+      end
     end
+  end
+  CardProperty::SUPPORTING_EXPANSION_LIST.each do |expansion_name|
+    include_examples :expansion, expansion_name.to_s
   end
 end
